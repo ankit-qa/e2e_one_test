8d33c1b9593208c1ace5b245b96a117f937b7648
ankitshrivatri <ankit.shrivatri@akelius.de>
2018-08-29 17:42:17 +0200

    Browser-set up- Completed
    setting up env for username password
    
    broswer set up done
    
    removed the sleep

--- features/support/hooks.rb
+++ features/support/hooks.rb
@@ -1,17 +1,15 @@
-=begin
-@browser = Watir::Browser.new :chrome
-
-
-Before do
-  @browser = browser
-end
-
-at_exit do
-  @browser.close
-end
-
-After do
-  @browser.cookies.clear
-end
-
-=end
+# @browser = Watir::Browser.new :chrome
+#
+#
+# Before do
+#   @browser = browser
+# end
+#
+# at_exit do
+#   @browser.close
+# end
+#
+# After do
+#   @browser.cookies.clear
+# end
+#
